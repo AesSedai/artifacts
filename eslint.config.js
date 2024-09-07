af79374811d51a36a7ef3ee0aac3a0622ce07a3f
Aes Sedai <zrlovin@gmail.com>
2024-09-07 13:03:20 -0700

    Another eslint config update

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,20 @@
-import tseslint from "typescript-eslint"
+import tseslint, { configs } from "typescript-eslint"
 import js from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier"
+import unusedImports from "eslint-plugin-unused-imports"
+import pluginPromise from "eslint-plugin-promise"
+import eslintPluginImportX from "eslint-plugin-import-x"
 
 export default tseslint.config(
     js.configs.recommended,
-    ...tseslint.configs.recommended,
+    ...configs.recommended,
     eslintConfigPrettier,
+    pluginPromise.configs["flat/recommended"],
+    eslintPluginImportX.flatConfigs.recommended,
+    eslintPluginImportX.flatConfigs.typescript,
+    {
+        plugins: { "unused-imports": unusedImports }
+    },
     {
         ignores: [".yarn/", "src/api/"]
     },
